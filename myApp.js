b4de29f62e8702a93b81a551409d76930b531e2b
Aida Ibisevic <72335194+aidaibisevic@users.noreply.github.com>
2021-03-14 10:32:26 +0100

    Challenge 1

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
-require('dotenv').config();
+const mongoose = require('mongoose');
 
+// 1) Install and Set Up Mongoose
+mongoose.connect(process.MONGO_URI.env, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+require('dotenv').config();
 
 let Person;
 
@@ -24,7 +31,7 @@ const findPersonById = (personId, done) => {
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+  const foodToAdd = 'hamburger';
 
   done(null /*, data*/);
 };
@@ -40,13 +47,13 @@ const removeById = (personId, done) => {
 };
 
 const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
+  const nameToRemove = 'Mary';
 
   done(null /*, data*/);
 };
 
 const queryChain = (done) => {
-  const foodToSearch = "burrito";
+  const foodToSearch = 'burrito';
 
   done(null /*, data*/);
 };
